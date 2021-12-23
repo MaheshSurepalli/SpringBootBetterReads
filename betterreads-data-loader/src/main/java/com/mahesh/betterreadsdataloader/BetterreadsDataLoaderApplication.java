183a09f0d468bd7c15580fbaec68ab2b8535411a
MaheshSurepalli <surepallimahesh8@gmail.com>
2021-12-23 19:59:18 +0530

    Loading the data to Cassandra

--- betterreads-data-loader/src/main/java/com/mahesh/betterreadsdataloader/BetterreadsDataLoaderApplication.java
+++ betterreads-data-loader/src/main/java/com/mahesh/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -1,6 +1,11 @@
 package com.mahesh.betterreadsdataloader;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 import javax.annotation.PostConstruct;
 
@@ -8,13 +13,17 @@ import com.mahesh.betterreadsdataloader.author.Author;
 import com.mahesh.betterreadsdataloader.author.AuthorRepository;
 import com.mahesh.betterreadsdataloader.connection.DataStaxAstraProperties;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties.Stream;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Lazy;
+
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
@@ -22,15 +31,41 @@ public class BetterreadsDataLoaderApplication {
    
     @Autowired
     AuthorRepository authorRepository;
+    @Value("${datadump.location.author}")
+    private String authorDumpLocation;
+    @Value("${datadump.location.works}")
+    private String worksDumpLocation;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
 	}
 
+    private void initAuthors() throws JSONException{
+       
+       try(FileInputStream fis = new FileInputStream(authorDumpLocation)){
+           Scanner sc = new Scanner(fis);
+           while(sc.hasNextLine()){
+               JSONObject line = new JSONObject(sc.nextLine().substring(sc.nextLine().indexOf("{")));
+               Author author = Author.builder().Id(line.optString("key").replace("/authors/", ""))
+                                .name(line.optString("name"))
+                                .personalName(line.optString("personal_name"))
+                                .build();
+                authorRepository.save(author);
+           }
+           sc.close();
+       }
+       catch(IOException e){
+           e.printStackTrace();
+       }
+
+    }
+    private void initWorks(){
+
+    }
     @PostConstruct
-    public void start(){
-        Author author = Author.builder().Id("id").name("name").personalName("personalname").build();
-        authorRepository.save(author);
-        System.out.println("Application is Started");
+    public void start() throws JSONException{
+        initAuthors();
+        initWorks();
     }
 
 	@Bean
--- betterreads-data-loader/src/main/java/com/mahesh/betterreadsdataloader/author/AuthorRepository.java
+++ betterreads-data-loader/src/main/java/com/mahesh/betterreadsdataloader/author/AuthorRepository.java
@@ -3,5 +3,6 @@ package com.mahesh.betterreadsdataloader.author;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AuthorRepository extends CassandraRepository<Author, String>{    
 }
